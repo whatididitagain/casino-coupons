4ad6606ee1f67441fd39888253e7be096ba7db10
whatididitagain <fobuliosmevraner@gmail.com>
2025-07-10 19:03:33 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'Arial' }}>
+      <h1>Casino Coupons</h1>
+      <p>Welcome to your freetips-style site. Content will load from your Strapi CMS soon!</p>
+    </div>
+  );
+}
